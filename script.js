409285e58a2f5c6cf94e333ecc8d05d9726bcf82
Baqar Mirza <144819270+baqarmirza1@users.noreply.github.com>
2023-11-28 10:57:19 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".mole1").click(function(){
+    $(".mole1").hide();
+});
+
+$(".whack1").click(function(){
+    $(".mole1").hide();
+});
+
+$(".whack2").dblclick(function(){
+    $(".mole-two").hide();
+});
+
+$(".whack3").click(function(){
+    $("img").show();
+    $(".third-mole").hide();
+});
+
+$(".reset").click(function(){
+    $("img").show();
+});
+
+$(".emptyhole").click(function(){
+    $("img").hide();
+});
